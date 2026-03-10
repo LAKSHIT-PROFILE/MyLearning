26a2f8edc00fdcdc0b0a53f18e77c7285a7e45aa
LAKSHIT-PROFILE <lakshitdhargave22@gmail.com>
2026-03-10 14:08:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+// const fs = require("node:fs");
+// //writefile
+// //appendfile
+// //copy file
+// //rename
+// //unlink
+
+// fs.writeFile("hey.txt", "hey hello kaise ho", function (err) {
+//   if (err) {
+//     console.log("error hai");
+//   } else {
+//     console.log("file written successfully");
+//   }
+// });
+// fs.appendFile("hey.txt", "---aur kya chal raha hai", function (err) {
+//   if (err) {
+//     console.log("error hai");
+//   } else {
+//     console.log("file appended successfully");
+//   }
+// });
+// fs.rename("hey.txt", "hello.txt", function (err) {
+//   if (err) {
+//     console.log("error hai");
+//   } else {
+//     console.log("file renamed successfully");
+//   }
+// });
+// // fs.copyFile("hello.txt", "copy.txt", function (err) {
+// //   if (err) {
+// //     console.log("error hai");
+// //   } else {
+// //     console.log("file copied successfully");
+// //   }
+// // });
+// fs.unlink("copy.txt", function (err) {
+//   if (err) {
+//     console.log("error hai");
+//   } else {
+//     console.log("file deleted successfully");
+//   }
+// });
+// fs.rmdir("heloo", function (err) {
+//   if (err) {
+//     console.log("error hai");
+//   } else {
+//     console.log("folder deleted successfully");
+//   }
+// });
+// fs.mkdir("heloo", function (err) {
+//   if (err) {
+//     console.log("error hai");
+//   } else {
+//     console.log("folder created successfully");
+//   }
+// });
+
+// http and https module
+//protocol=rules
+//create server
+
+// const http = require("http");
+// const server = http.createServer(function (req, res) {
+//   res.end("hello world");
+// });
+// server.listen(3000);
+
+//npm understanding--
+//node js core  mein jo installed aata hai wo kehlata hai module
+//npm se download karte hai wo hota hai packages
+
+// text to speech
+
+//npm node package manager
+//installing and uninstalling packages anything basic and advanced
+//understanding node modules
+//dependencies
+//devdependencies
+//scripts-understanding defualt scripts path and custom scripts
